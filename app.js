28b92696f41a3cb441c5109f3b17402b94ae0b03
徐仁禄 <xurenlu@gmail.com>
2022-01-28 17:38:50 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+const {Direction, MessageBus, SocketBus} = require( "node-gotapi-ws");
+const channel = "/valine-comment/notifier/hpq1SCEJulwBRQy0u8fnhX0Y-9Nh9j0Va"
+require('dotenv').config();
+const mail = require(  "./utilities/send-mail")
+const wsHost = "ws.gotapi.net"
+let socketBus = new SocketBus(`wss://${wsHost}${channel}`);
+let messageBus = new MessageBus(Direction.Customer, socketBus, `https://${wsHost}/__/history/`,
+    `${channel}`, 1, 0)
+//设置一个回调，在有新消息到达时，会被触发。
+
+ messageBus.setNewMessageCallback(
+     (newMsg, uuid) => {
+         //console.log("new msg arrived")
+         let data = JSON.parse(newMsg.data);
+         console.log(data)
+         //let comment = new Comment(JSON.parse(newMsg.data));
+         mail.notice(data)
+     }
+ );
---
+++ utilities/send-mail.js
@@ -0,0 +1,72 @@
+'use strict';
+const nodemailer = require('nodemailer');
+const ejs = require('ejs');
+const fs  = require('fs');
+const path = require('path');
+
+let config = {
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASS
+    }
+}
+
+if (process.env.SMTP_SERVICE != null) {
+    config.service = process.env.SMTP_SERVICE;
+} else {
+    config.host = process.env.SMTP_HOST;
+    config.port = parseInt(process.env.SMTP_PORT);
+    config.secure = process.env.SMTP_SECURE === "false" ? false : true;
+}
+console.log(process.env)
+console.log(config)
+const transporter = nodemailer.createTransport(config);
+let templateName = process.env.TEMPLATE_NAME ?  process.env.TEMPLATE_NAME : "default";
+let noticeTemplate = ejs.compile(fs.readFileSync(path.resolve(process.cwd(), 'template', templateName, 'notice.ejs'), 'utf8'));
+// 提醒站长
+exports.notice = (comment) => {
+    console.warn("notice called")
+    console.log(comment)
+    // 站长自己发的评论不需要通知
+    if (comment.mail === process.env.TO_EMAIL
+        || comment.mail === process.env.SMTP_USER) {
+        console.log("自己的评论不需要邮件通知")
+        //return;
+    }
+
+    console.log("38")
+    let emailSubject = '👉 咚！「' + process.env.SITE_NAME + '」上有新评论了';
+    let emailContent =  noticeTemplate({
+                            siteName: process.env.SITE_NAME,
+                            siteUrl: process.env.SITE_URL,
+                            name: comment.nick,
+                            text: comment.comment,
+                            url: process.env.SITE_URL + comment.url
+                        });
+
+    let mailOptions = {
+        from: '"' + process.env.SENDER_NAME + '" <' + process.env.SMTP_USER + '>',
+        to: process.env.TO_EMAIL ? process.env.TO_EMAIL : process.env.SMTP_USER,
+        subject: emailSubject,
+        html: emailContent
+    };
+    console.log("mailOptions")
+    console.log(mailOptions)
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log("收到一条评论, 已提醒站长");
+    })
+}
+
+// 该方法可验证 SMTP 是否配置正确
+exports.verify = function(){
+    console.log("....");
+    transporter.verify(function(error, success) {
+        if (error) {
+            console.log(error);
+        }
+        console.log("Server is ready to take our messages");
+    });
+};
